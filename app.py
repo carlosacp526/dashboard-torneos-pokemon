5a6b2a5648a85bc4922ce23386a51e039987a936
Carlos <carlosacp526@gmail.com>
2025-11-28 08:56:12 -0500

    Descripcion del cambio

--- app.py
+++ app.py
@@ -895,16 +895,16 @@ if player_query:
     
     with col_img:
         # Intentar cargar la imagen del jugador
-        imagen_path = f"jugadores/{player_query.lower().replace(' ', '_')}.png"
+        imagen_path = f"jugadores/{player_query.replace(' ', '_')}.png"
         try:
             st.image(imagen_path, width=200, caption=player_query)
         except:
             try:
-                imagen_path = f"jugadores/{player_query.lower().replace(' ', '_')}.jpeg"    
+                imagen_path = f"jugadores/{player_query.replace(' ', '_')}.jpeg"    
                 st.image(imagen_path, width=200, caption=player_query)
             except:           
                 try:
-                    imagen_path = f"jugadores/{player_query.lower().replace(' ', '_')}.jpg"    
+                    imagen_path = f"jugadores/{player_query.replace(' ', '_')}.jpg"    
                     st.image(imagen_path, width=200, caption=player_query)
                 except:
                     st.info("📷 Imagen no disponible")
