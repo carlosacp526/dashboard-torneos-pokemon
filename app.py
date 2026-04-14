0d72bf375b96856ff0c6cadd0cb37fa89f9492b6
Carlos <carlosacp526@gmail.com>
2026-04-13 22:09:17 -0500

    Descripcion del cambio

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ sys.path.insert(0, ROOT)
 
 st.set_page_config(page_title="Poketubi Dashboard", layout="wide", page_icon="⚡")
 
-from vistas import inicio, analisis, jugadores, rankings, elo, ligas, prediccion
+from Produccion.vistas import jugadoresfinal
+from vistas import inicio, analisis, rankings, elo, ligas, prediccion
 from vistas import calidad
 p_calidad = st.Page(calidad.show, title="🔬 Calidad de Ligas", url_path="calidad")
 p_inicio     = st.Page(inicio.show,     title="🏠 Inicio",                  url_path="inicio",     default=True)
 p_analisis   = st.Page(analisis.show,   title="📊 Análisis General",        url_path="analisis")
-p_jugadores  = st.Page(jugadores.show,  title="👤 Jugadores",               url_path="jugadores")
+p_jugadores  = st.Page(jugadoresfinal.show,  title="👤 Jugadores",               url_path="jugadores")
 p_ligas      = st.Page(ligas.show,      title="🏆 Ligas y Torneos",         url_path="ligas")
 p_rankings   = st.Page(rankings.show,   title="🏅 Rankings",                url_path="rankings")
 p_elo        = st.Page(elo.show,        title="⚡ Ranking Elo",             url_path="elo")
