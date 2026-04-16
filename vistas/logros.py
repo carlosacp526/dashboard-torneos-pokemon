d04c90d68c778c9f3b2976b9901ae3459eb40f34
Carlos <carlosacp526@gmail.com>
2026-04-16 01:11:26 -0500

    Descripcion del cambio

--- vistas/logros.py
+++ vistas/logros.py
@@ -456,7 +456,7 @@ def evaluar_logros(
     CAMPEONES_TORNEO = [
         "Yabadaba", "MaskWolf","Chino","The.Ultracheese","Luigillanos","Renzo","Alechiii","Aikauwu","D'Allfather","Haseo","Joscake","A25","Angello77","Nigga Chan",
         "Davarv","haiseowo","David Wong","Valentino Parra","Fur4nko","Moirix","LABIAMG","Skll02","Darmanethan","RIIZExyz","Hydreigon_chelas","Saperoko10","2DpkmN",
-        "Mr.Shadowdusk","Chris FPS","Adpg"
+        "Mr.Shadowdusk","Chris FPS","Adpg","SasoriVzla7"
         # agregar más aquí
     ]
     def _verdugo_elite():
