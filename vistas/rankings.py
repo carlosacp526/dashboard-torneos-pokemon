7ea1922e5257d0d0be6a1dd3ebf3dbb8fffee506
Carlos <carlosacp526@gmail.com>
2026-07-02 10:06:41 -0500

    Descripcion del cambio

--- vistas/rankings.py
+++ vistas/rankings.py
@@ -43,6 +43,7 @@ def show():
     st.header("📈 Ranking Elo")
 
     meses_elo = [
+         ("Junio26.png",    "Junio 26"),
          ("Mayo26.png",    "Mayo 2026"),
         ("Abril26.png",    "Abril 2026"),
         ("Marzo26.png",    "Marzo 2026"),
