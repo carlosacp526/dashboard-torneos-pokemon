747311c487979b9851603fe3fa83a088a3ca4673
Carlos <carlosacp526@gmail.com>
2026-09-08 00:47:14 -0500

    Descripcion del cambio}

--- vistas/utils.py
+++ vistas/utils.py
@@ -90,7 +90,7 @@ def asignar_zona(rank, total, lt):
         if rank in [2,3]: return "Ascenso"
         if rank > total-3: return "Descenso"
         return ""
-    if lt in ('PJST3','PJST4','PJST5'):
+    if lt in ('PJST3','PJST4','PJST5','PJST6'):
         if rank == 1: return "Líder"
         if rank in [2,3]: return "Ascenso"
         if rank > total-2: return "Descenso"
